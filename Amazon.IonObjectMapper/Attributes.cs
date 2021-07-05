9fca70466e9de00def49856a9602e348caa2f4d6
Justin Gossett <justing@bitquilltech.com>
2021-07-05 13:54:26 -0700

    Add docstrings for Attributes.cs

--- Amazon.IonObjectMapper/Attributes.cs
+++ Amazon.IonObjectMapper/Attributes.cs
@@ -2,19 +2,46 @@
 
 namespace Amazon.IonObjectMapper
 {
+    /// <summary>
+    /// Attribute to identify a .NET property that should be ignored
+    /// by the serializer.
+    /// </summary>
     public class IonIgnore : Attribute
     {
     }
 
+    /// <summary>
+    /// Attribute to identify a .NET type to target during deserialization.
+    /// </summary>
     public class IonAnnotateType : Attribute
     {
+        /// <summary>
+        /// Flag to indicate that whether any classes descending from the annotated class
+        /// are excluded from the annotation.
+        /// </summary>
         public bool ExcludeDescendants { get; init; }
+        
+        /// <summary>
+        /// The .NET namespace of the annotated type.
+        /// </summary>
         public string Prefix { get; set; }
+        
+        /// <summary>
+        /// The .NET class name of the annotated type.
+        /// </summary>
         public string Name { get; set; }
     }
 
+    /// <summary>
+    /// Attribute to identify a .NET type that should not be annotated
+    /// even if a parent class is annotated.
+    /// </summary>
     public class IonDoNotAnnotateType : Attribute
     {
+        /// <summary>
+        /// Flag to indicate that whether any classes descending from the annotated class
+        /// are excluded from the IonAnnotateType annotation.
+        /// </summary>
         public bool ExcludeDescendants { get; init; }
     }
 
@@ -29,6 +56,7 @@ namespace Amazon.IonObjectMapper
         /// to create IonSerializerFactory with custom context
         /// </summary>
         public Type Factory { get; init; }
+        
         /// <summary>
         /// The name of the IonSerializer Attribute to be used 
         /// to create custom IonSerializer
@@ -38,19 +66,29 @@ namespace Amazon.IonObjectMapper
 
     /// <summary>
     /// Attribute to identify a custom constructor to be used to instantiate instances
-    /// of the class annotated with this attribute during deserialization.
+    /// of classes annotated with this attribute during deserialization.
     /// </summary>
     public class IonConstructor : Attribute
     {
     }
 
+    /// <summary>
+    /// Attribute to identify the Ion field name to be used during serialization and/or
+    /// deserialization of a .NET property annotated with this attribute.
+    /// </summary>
     public class IonPropertyName : Attribute
     {
+        /// <summary>
+        /// IonPropertyName constructor.
+        /// </summary>
         public IonPropertyName(string name)
         {
             Name = name;
         }
 
+        /// <summary>
+        /// The Ion field name to be used instead of the .NET property's name.
+        /// </summary>
         public string Name { get; }
     }
 
@@ -94,6 +132,10 @@ namespace Amazon.IonObjectMapper
         public string IonPropertyName { get; }
     }
 
+    /// <summary>
+    /// Attribute to identify a .NET field to be included during
+    /// serialization and deserialization.
+    /// </summary>
     public class IonField : Attribute
     {
     }
